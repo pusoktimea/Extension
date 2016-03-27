eceef30f6d70822a58a9ae3a1dd1939c7721464b
pusoktimea <pusoktimea@users.noreply.github.com>
2016-03-28 00:25:45 +0300

    Added files via upload

--- popup.js
+++ popup.js
@@ -1,49 +1,52 @@
-//when the extension is loaded, this function will hide the content of the session elements,
-//which has to be displayed just after click on the "Start Session" button. 
-$(document ).ready(function(){
-  $('#session').hide();
+  //when the extension is loaded, this function will hide the content of the session elements,
+  //which has to be displayed just after click on the "Start Session" button. 
+  $(document ).ready(function(){
+    $('#session').hide();
 
-//when "Start Session" button is clicked the function will show the session elements
-//and hide the div with the informations about area charter tester and environment 
+  //when "Start Session" button is clicked the function will show the session elements
+  //and hide the div with the informations about area charter tester and environment 
 
-  $(function() {
-    $('#startSessionButton').click(function(){
-      $('#session').show();
-      $('#generalInfo').hide();
+    $(function() {
+      $('#startSessionButton').click(function(){
+        $('#session').show();
+        $('#generalInfo').hide();
+      });
     });
-  });
+
+
+    //navigate between textboxes after starting the session,using up-down arrows
+   var first_element=$("#textboxes li:first-child");
+   var last_element=$("#textboxes li:last-child")
+
+   first_element.addClass("current").fadeIn();
 
   $('.target').keydown(function(e){
-    if (e.keyCode == 39) { 
-      $(this).parent().fadeOut();
-      $(this).parent().next('div').fadeIn();
-      $(this).parent().next('div').find('input').val('').focus();
-      return false;
+    if (e.keyCode == 40) { 
+    var current_div=$("#textboxes li.current");
+    current_div.removeClass("current").fadeOut();
+
+    if(!current_div.is("#textboxes li:last-child")){
+      current_div=current_div.next('li').addClass("current").fadeIn().find('input').val('').focus();
+      
+    }else{
+      current_div=first_element.addClass("current").fadeIn().find('input').val('').focus();
     }
-    else if(e.keyCode == 37){
-      $(this).parent().fadeOut();
-      $(this).parent().prev('div').fadeIn();
-      $(this).parent().prev('div').find('input').val('').focus();
-      return false;
+  }
+
+   else if(e.keyCode == 38){
+  var current_div=$("#textboxes li.current");
+    current_div.removeClass("current").fadeOut();
+
+    if(!current_div.is("#textboxes li:first-child")){
+      current_div=current_div.prev('li').addClass("current").fadeIn().find('input').val('').focus();
+      
+    }else{
+      current_div=last_element.addClass("current").fadeIn().find('input').val('').focus();
     }
+       }
+   });
+//navigation ends
+
+    
+
   });
-
-  $('#next').click(function() {
-    $('.current').removeClass('current').hide().next().show().addClass('current');
-    if ($('.current').hasClass('last')) {
-      $('#next').attr('disabled', true);
-    }
-    $('#prev').attr('disabled', null);
-  });
-
-  $('#prev').click(function() {
-    $('.current').removeClass('current').hide().prev().show().addClass('current');
-    if ($('.current').hasClass('first')) {
-      $('#prev').attr('disabled', true);
-    }
-    $('#next').attr('disabled', null);
-  });
-
-
-});
-
