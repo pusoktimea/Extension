0715196874691547c2d7799c0ad9a402fe97e116
pusoktimea <pusoktimea@users.noreply.github.com>
2016-04-01 10:47:08 +0300

    Added files via upload

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@
   $(document ).ready(function(){
     $('#session').hide();
 
+      
   //when "Start Session" button is clicked the function will show the session elements
   //and hide the div with the informations about area charter tester and environment 
 
@@ -10,30 +11,35 @@
       $('#startSessionButton').click(function(){
         $('#session').show();
         $('#generalInfo').hide();
+        
       });
     });
 
 
     //navigate between textboxes after starting the session,using up-down arrows
    var first_element=$("#textboxes li:first-child");
-   var last_element=$("#textboxes li:last-child")
+   var last_element=$("#textboxes li:last-child");
+
+   //var first-icon=$("#icons-list li:first-child");
+   //var last-icon=$("#icons-list li:last-child");
 
    first_element.addClass("current").fadeIn();
+  // first-icon.addClass("current-icon").css("background","red");
 
   $('.target').keydown(function(e){
-    if (e.keyCode == 40) { 
+    if (e.keyCode == 39) { 
     var current_div=$("#textboxes li.current");
     current_div.removeClass("current").fadeOut();
 
     if(!current_div.is("#textboxes li:last-child")){
       current_div=current_div.next('li').addClass("current").fadeIn().find('input').val('').focus();
-      
+    
     }else{
       current_div=first_element.addClass("current").fadeIn().find('input').val('').focus();
     }
   }
 
-   else if(e.keyCode == 38){
+   else if(e.keyCode == 37){
   var current_div=$("#textboxes li.current");
     current_div.removeClass("current").fadeOut();
 
@@ -47,6 +53,32 @@
    });
 //navigation ends
 
+//icons
+// $(function(){
+//   $("#input-setup").focus(function(){
+//     $(".circle-icon").css("background","red");
+//   })
+//   .blur(function(){
+//     $(this).css("background","transparent");
+//   })
+ });
+
+
+
+
+//progress bar
+// function progress(timeleft, timetotal, $element) {
+//     var progressBarWidth = timeleft * $element.width() / timetotal;
+//     $element.find('div').animate({ width: progressBarWidth }, 2500).html(timeleft + "");
+//     if(timeleft > 0) {
+//         setTimeout(function() {
+//             progress(timeleft - 1, timetotal, $element);
+//         }, 60000);
+//     }
+// };
+
+// progress(60, 60, $('#progressBar'));
     
 
-  });
+//   });
+  
